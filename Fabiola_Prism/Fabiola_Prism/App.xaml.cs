c12ce862e07686466d0c9b6a3842fc574cbbefea
Fabíola Martins <fabiola.andrade.martins@outlook.com>
2024-09-25 15:48:03 +0100

    Add project files.

---
+++ Fabiola_Prism/Fabiola_Prism/App.xaml.cs
@@ -0,0 +1,33 @@
+using Fabiola_Prism.ViewModels;
+using Fabiola_Prism.Views;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace Fabiola_Prism
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/LoginPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();            
+            containerRegistry.RegisterForNavigation<LoginPage, LoginPageViewModel>();
+        }
+    }
+}
